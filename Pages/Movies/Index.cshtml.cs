26e13afccd6e5cf55e54fc7cc9b59ddd3e112ed6
Ryze <1053289617@qq.com>
2020-06-05 17:55:41 +0800

    Added search by genre and title

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -19,11 +20,42 @@ namespace RazorPagesMovie.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+
+        //若要将属性绑定在 GET 请求上，请将 [BindProperty] 特性的 SupportsGet 属性设置为 true
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            //Movie = await _context.Movie.ToListAsync();
+
+            //var movies = from m in _context.Movie select m;
+            //if (!string.IsNullOrEmpty(SearchString))
+            //{
+            //    movies = movies.Where(movie => movie.Title.Contains(SearchString));
+            //}
+            //Movie = await movies.ToListAsync()  ;
+
+            //从数据库中搜索所有的Genre(类型)
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+            
+            var movies = from m in _context.Movie select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(movie => movie.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
